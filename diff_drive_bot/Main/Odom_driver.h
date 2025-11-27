892b920b3ce038d28d92afde5b4d0c01ce7da4c3
NguyenChiCuong-1706 <145039684+NguyenChiCuong-1706@users.noreply.github.com>
2025-11-27 17:55:25 +0800

    Delete diff_drive_bot/Main directory

--- diff_drive_bot/Main/Odom_driver.h
+++
@@ -1,6 +0,0 @@
-// #ifdef ODOM_CAL
-//   #define R_wheel 31          //Wheel diameter (mm)
-//   #define d_baseline 40       //Distance between two wheels (mm)  
-// #endif
-
-// void Odometry(int16_t delta_left_pul, int16_t delta_right_pul);
--- diff_drive_bot/Main/Radio_driver.h
+++
@@ -1,6 +0,0 @@
-#ifdef RADIO_FLYSKY_I6
-
-#endif
-
-void receive() ;
-void RF_read(int *des_m1, int *des_m2);
